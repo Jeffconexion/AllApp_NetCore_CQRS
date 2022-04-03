1405f87cba3855f9360a07006746f1943f9ee3b0
Jefferson Santos <jeff.debian2016@gmail.com>
2022-04-03 16:07:01 -0300

    Create class todoItem for a new task.

---
+++ AppToDo/src/AppTodo.Core/Entities/TodoItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AppTodo.Core.Entities
+{
+  public class TodoItem : EntityBase
+  {
+    public string Title { get; private set; }
+    public bool Done { get; private set; }
+    public DateTime Date { get; private set; }
+    public string User { get; private set; } //external user from google.
+
+    public TodoItem(string title, bool done, DateTime date, string user)
+    {
+      Title = title;
+      Done = done;
+      Date = date;
+      User = user;
+    }
+
+    public void MarkAsDone()
+    {
+      Done = true;
+    }
+
+    public void MarkAsUndone()
+    {
+      Done = false;
+    }
+
+    public void UpdateTitle(string title)
+    {
+      Title = title;
+    }
+
+  }
+}
