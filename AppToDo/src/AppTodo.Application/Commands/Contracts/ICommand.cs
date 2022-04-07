da1082a23c0ddbe93783a79fc9e6391c1d70b728
Jefferson Santos <jeff.debian2016@gmail.com>
2022-04-07 08:35:17 -0300

    Use flunt at contract.

---
+++ AppToDo/src/AppTodo.Application/Commands/Contracts/ICommand.cs
@@ -0,0 +1,17 @@
+﻿using Flunt.Validations;
+
+namespace AppTodo.Application.Commands.Contracts
+{
+  /// <summary>
+  /// @author: Jefferson Santos
+  /// @Data  : 07/04/2022
+  /// 
+  /// Flunt: version 1.4
+  /// All commands I force to hava a contract with ICommand.
+  /// Where I validate values.
+  /// </summary>
+  public interface ICommand : IValidatable
+  {
+
+  }
+}
