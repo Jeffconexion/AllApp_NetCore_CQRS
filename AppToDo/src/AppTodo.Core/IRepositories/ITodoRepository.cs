5231110f0ee8b6a6ea83b5531a269a66cfc39923
Jefferson Santos <jeff.debian2016@gmail.com>
2022-04-12 05:50:15 -0300

    Repository Pattern.

---
+++ AppToDo/src/AppTodo.Core/IRepositories/ITodoRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using AppTodo.Core.Entities;
+
+namespace AppTodo.Core.IRepositories
+{
+  /// <summary>
+  /// @author: Jefferson Santos
+  /// @Data  : 12/04/2022
+  /// 
+  /// Interface repository pattern responsible for data persistence.
+  /// </summary>
+  public interface ITodoRepository
+  {
+    /// <summary>
+    /// Create new task.
+    /// </summary>
+    /// <param name="todo">Object of the type TodoItem</param>
+    /// <returns></returns>
+    Task Create(TodoItem todo);
+
+    /// <summary>
+    /// Update task.
+    /// </summary>
+    /// <param name="todo">Object of the type TodoItem</param>
+    /// <returns></returns>
+    Task Update(TodoItem todo);
+
+    /// <summary>
+    /// Delete task
+    /// </summary>
+    /// <param name="Id">Id of the object.</param>
+    /// <returns></returns>
+    Task Delete(Guid Id);
+  }
+}
