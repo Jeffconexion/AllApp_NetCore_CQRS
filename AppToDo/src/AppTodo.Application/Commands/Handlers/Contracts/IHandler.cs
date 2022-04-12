6727151b8e65f69b156d975bf4dba307dad995ae
Jefferson Santos <jeff.debian2016@gmail.com>
2022-04-12 05:16:29 -0300

    Interface handler

---
+++ AppToDo/src/AppTodo.Application/Commands/Handlers/Contracts/IHandler.cs
@@ -0,0 +1,22 @@
+﻿using AppTodo.Application.Commands.Contracts;
+
+namespace AppTodo.Application.Commands.Handlers.Contracts
+{
+  /// <summary>
+  /// @author: Jefferson Santos
+  /// @Data  : 12/04/2022
+  /// 
+  /// Interface IHandler.
+  /// only propor is a group of object
+  /// </summary>
+  public interface IHandler<T> where T : ICommand
+  {
+    /// <summary>
+    /// Return result of the execution handler.
+    /// </summary>
+    /// <param name="command">object type of ICommand</param>
+    /// <returns></returns>
+    ICommandResult Handle(T command);
+
+  }
+}
