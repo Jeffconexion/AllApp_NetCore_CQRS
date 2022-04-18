4cbbc01a1f51bc69bc7293cb42efcc318686e342
Jefferson Santos <jeff.debian2016@gmail.com>
2022-04-18 16:05:36 -0300

    Tests commands.

---
+++ AppToDo/tests/AppTodo.Tests/CommandTests/CreateTodoCommandTests/CreateCommandTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using AppTodo.Application.Commands;
+using FluentAssertions;
+using Xunit;
+
+namespace AppTodo.Tests.CommandTests.CreateTodoCommandTests
+{
+  /// <summary>
+  /// @author: Jefferson Santos
+  /// @Data  : 07/04/2022
+  /// 
+  /// Tests my command CreateTodoCommandTests.
+  /// </summary>
+  public class CreateCommandTests
+  {
+    [Fact(DisplayName = "Check if command is invalid.")]
+    [Trait("Commands", "To do many tests with command.")]
+    public void CreateTodo_Command_Invalid()
+    {
+      // arrange
+      CreateTodoCommand command = new CreateTodoCommand("", "", DateTime.Now);
+
+      //act
+      command.Validate();
+
+      //assert
+      command.Valid.Should().BeFalse();
+
+    }
+
+    [Fact(DisplayName = "Check if command is invalid.")]
+    [Trait("Commands", "To do many tests with command.")]
+    public void CreateTodo_Command_valid()
+    {
+      // arrange
+      CreateTodoCommand command = new CreateTodoCommand("Teste", "Carlos", DateTime.Now);
+
+      //act
+      command.Validate();
+
+      //assert
+      command.Valid.Should().BeTrue();
+
+    }
+
+    [Fact(DisplayName = "Check if command is invalid.")]
+    [Trait("Commands", "To do many tests with command.")]
+    public void CreateTodo_Command_InvalidNotifications()
+    {
+      // arrange
+      CreateTodoCommand command = new CreateTodoCommand("", "", DateTime.Now);
+
+      //act
+      command.Validate();
+
+      //assert
+      command.Notifications.Should().NotBeEmpty().And.HaveCount(2);
+
+    }
+
+    [Fact(DisplayName = "Check if command is valid.")]
+    [Trait("Commands", "To do many tests with command.")]
+    public void CreateTodo_Command_ValidNotifications()
+    {
+      // arrange
+      CreateTodoCommand command = new CreateTodoCommand("Teste", "Carlos", DateTime.Now);
+
+      //act
+      command.Validate();
+
+      //assert
+      command.Notifications.Should().BeNullOrEmpty().And.HaveCount(0);
+
+    }
+  }
+}
