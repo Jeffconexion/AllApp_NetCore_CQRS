1e7a434d34e39ef84f07e30d362610af34a8c29e
Jefferson Santos <jeff.debian2016@gmail.com>
2022-04-18 15:20:01 -0300

    Tests strings and idGuid.

---
+++ AppToDo/tests/AppTodo.Tests/EntityTests/TodoItemTests/IdGuidTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using AppTodo.Core.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace AppTodo.Tests.EntityTests.TodoItemTests
+{
+  /// <summary>
+  /// @author: Jefferson Santos
+  /// @Data  : 18/04/2022
+  /// 
+  /// Test the type Guid.
+  /// </summary>
+  public class IdGuidTest
+  {
+    [Fact(DisplayName = "Check Guid")]
+    [Trait("TodoItem", "To do many tests in entity.")]
+    public void Number_Check_NotBeEmpty()
+    {
+      //arrange
+      TodoItem todoItem = new TodoItem("Teste", "Carlos", DateTime.Now);
+
+      //assert
+      todoItem.Id.Should().NotBeEmpty().And.NotBe(Guid.Empty);
+    }
+
+    [Fact(DisplayName = "Check Guid")]
+    [Trait("TodoItem", "To do many tests in entity.")]
+    public void Number_Check_Teste()
+    {
+      //arrange
+      TodoItem todoItem = new TodoItem("Teste", "Carlos", DateTime.Now);
+
+      //act
+      var resultado = todoItem.Id;
+
+      //assert
+      resultado.ToByteArray().Should().HaveCount(16);
+    }
+
+  }
+}
---
+++ AppToDo/tests/AppTodo.Tests/EntityTests/TodoItemTests/StringsTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using AppTodo.Core.Entities;
+using FluentAssertions;
+using Xunit;
+
+namespace AppTodo.Tests.EntityTests.TodoItemTests
+{
+  /// <summary>
+  /// @author: Jefferson Santos
+  /// @Data  : 18/04/2022
+  /// 
+  /// Test the type strings.
+  /// </summary>
+  public class StringsTests
+  {
+    [Fact(DisplayName = "Check title")]
+    [Trait("TodoItem", "To do many tests in entity.")]
+    public void Strings_CheckTitle_ReturnTitle()
+    {
+      //arrange
+      TodoItem todoItem = new TodoItem("Teste", "Carlos", DateTime.Now);
+
+      //assert
+      todoItem.Title.Should().Contain("Teste");
+    }
+
+    [Fact(DisplayName = "Check User")]
+    [Trait("TodoItem", "To do many tests in entity.")]
+    public void Strings_CheckUser_ReturnUser()
+    {
+      //arrange
+      TodoItem todoItem = new TodoItem("Teste", "Carlos", DateTime.Now);
+
+      //assert
+      todoItem.User.Should().Contain("Carlos");
+    }
+  }
+}
