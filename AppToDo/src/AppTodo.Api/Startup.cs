fec9e74ae57d8888e8ba82c07dfedca1670c5541
Jefferson Santos <jeff.debian2016@gmail.com>
2022-05-11 06:58:43 -0300

    Configuration class startup and dependencs.

--- AppToDo/src/AppTodo.Api/Startup.cs
+++ AppToDo/src/AppTodo.Api/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using AppTodo.Infrastructure.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,12 @@ using Microsoft.OpenApi.Models;
 
 namespace AppTodo.Api
 {
+  /// <summary>
+  /// @author: Jefferson Santos
+  /// @Data  : 11/05/2022
+  /// 
+  /// Set and Dependences.
+  /// </summary>
   public class Startup
   {
     public Startup(IConfiguration configuration)
@@ -17,19 +24,22 @@ namespace AppTodo.Api
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
 
       services.AddControllers();
-      services.AddDbContext<DataContext>
+      services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));//the same addScoped, but specify to database.
+      //services.AddDbContext<DataContext>(opt =>
+      //{
+      //  opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+      //});
+
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "AppTodo.Api", Version = "v1" });
       });
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
